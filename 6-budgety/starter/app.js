24850552f54f5cbaef67e0b9efd9cca9f8e519ae
masleydean <masleydean@penguin>
2019-05-10 17:40:41 +0200

    update

---
+++ 6-budgety/starter/app.js
@@ -0,0 +1,21 @@
+var budgetController = (function() {
+
+	// some code
+
+})();
+
+
+
+var UIController = (function() {
+
+	// some code
+
+})();
+
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+	// some code
+
+})(budgetController, UIController);
